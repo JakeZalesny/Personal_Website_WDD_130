df4ee783b810730ddefdb06623ef48b8dafbfbb7
JakeZalesny <jrzalesny@gmail.com>
2022-12-03 23:03:23 -0700

    added js file, added css, added imgs

---
+++ merch-script.js
@@ -0,0 +1,11 @@
+function imgModifications(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+
+  var imgText = document.getElementById("imgtext");
+
+  expandImg.src = imgs.src;
+
+  imgText.innerHTML = imgs.alt;
+
+  expandImg.parentElement.style.display = "block";
+}
