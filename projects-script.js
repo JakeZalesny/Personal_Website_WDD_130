5afad2e6c6026c0b5e9aead2dcd006928b107bde
JakeZalesny <jrzalesny@gmail.com>
2022-12-10 20:44:20 -0700

    subpage updated and completed

---
+++ projects-script.js
@@ -0,0 +1,29 @@
+function playGraveyard() {
+  var audio = document.getElementById("graveyard-audio");
+  audio.play();
+}
+
+function stopGraveyard() {
+  var audio = document.getElementById("graveyard-audio");
+  audio.pause();
+}
+
+function playScars() {
+  var scars = document.getElementById("scars");
+  scars.play();
+}
+
+function stopScars() {
+  var scars = document.getElementById("scars");
+  scars.pause();
+}
+
+function playITILY() {
+  var ITILY = document.getElementById("i-think-i-like-you");
+  ITILY.play();
+}
+
+function stopITILY() {
+  var ITILY = document.getElementById("i-think-i-like-you");
+  ITILY.pause();
+}
